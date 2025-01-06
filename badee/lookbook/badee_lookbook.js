45dc4cb81910dee0e56a04cf5589cb7fb3b64cfd
limjuhee0803 <limjuhee0803@naver.com>
2025-01-06 19:36:21 +0900

    badee lookbook페이지 제작

--- badee/lookbook/badee_lookbook.js
+++ badee/lookbook/badee_lookbook.js
@@ -39,23 +39,6 @@ $('.up').on('click', () => {
   });
 });
 
-// banner 마우스 이벤트
-const img1 = document.querySelector('.item1');
-const img2 = document.querySelector('.item2');
-const img3 = document.querySelector('.item3');
-const img4 = document.querySelector('.item4');
-
-addEventListener('mousemove', (evt) => {
-  let x = evt.clientX,
-    y = evt.clientY;
-  console.log(x, y);
-
-  img1.style.transform = `translate(${x / 60}px,${-y / 50}px)`;
-  img2.style.transform = `translate(${x / -50}px,${-y / 100 + 10}px)`;
-  img3.style.transform = `translate(${30 - x / 60}px,${-y / 50}px)`;
-  img4.style.transform = `translate(${50 - x / 80}px,${-y / 20}px)`;
-});
-
 
 $('.footer-butterfly').hover(
   (e) => {
@@ -74,11 +57,11 @@ $('.footer-butterfly.active-portfolio').on('click', () => {
 });
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView : 'auto',
-  loop : true,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
+    slidesPerView: 14,
+    spaceBetween: 10,
+    centeredSlides:true,
+    navigation:{
+      nextEl:".swiper-button-next",
+      prevEl:".swiper-button-prev",
   },
 });
-

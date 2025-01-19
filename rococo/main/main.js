8c08361e1d820e039f5f96654379340b969beb03
limjuhee0803 <limjuhee0803@naver.com>
2025-01-19 18:47:38 +0900

    rococo main page

---
+++ rococo/main/main.js
@@ -0,0 +1,34 @@
+// 메인 배너 이벤트
+const inner = document.querySelector(".inner");
+        const section = document.querySelector("section");
+
+        window.onscroll = function() {
+        let value = window.pageYOffset / section.offsetTop + 1;
+        inner.style.transform = `scale(${value})`;
+        };
+
+    $(function(){
+    
+    var $window = $(window);    //Window object
+    
+    var scrollTime = 1.2;     //Scroll time
+    var scrollDistance = 170;   //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+        
+    $window.on("mousewheel DOMMouseScroll", function(event){
+        
+        event.preventDefault(); 
+                        
+        var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+        
+        TweenMax.to($window, scrollTime, {
+        scrollTo : { y: finalScroll, autoKill:true },
+            ease: Power1.easeOut,
+            autoKill: true,
+            overwrite: 5              
+        });
+            
+    });
+    
+    });

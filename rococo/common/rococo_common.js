c9d2c26771f6925b108558991d6baf609870f1ec
limjuhee0803 <limjuhee0803@naver.com>
2025-01-21 17:49:50 +0900

    스크롤 애니메이션 추가

--- rococo/common/rococo_common.js
+++ rococo/common/rococo_common.js
@@ -1,3 +1,4 @@
+
 //header
 let prevScroll = window.scrollY;
 
@@ -19,4 +20,35 @@ $('.hambuger').on('click', () => {
   $('.hambuger').toggleClass('change-hambuger');
 });
 
+
+    // 스크롤 애니메이션
+    $(function(){
+      function scrollevents(scrollPos){
+          var wh =  $(window).height();
+          current_scroll = scrollPos;
+          $(".show_trigger").each(function(){
+              show_whatever($(this), current_scroll, wh);
+          });
+      }
+
+      let ticking = false;
+      var current_scroll, wh;
+      function show_whatever(el, current_scroll, wh){
+          var el_offset_t = el.offset().top;
+          if(current_scroll > el_offset_t - wh + 150){
+              el.parent().removeClass("wait_scroll");
+              el.remove();
+          }    
+      }
+
+      $(window).on('scroll',function(){
+          if (!ticking) {
+              window.requestAnimationFrame(function() {
+                  scrollevents(window.scrollY);
+                  ticking = false;
+              });
+              ticking = true;
+          }
+      });
+  });
 

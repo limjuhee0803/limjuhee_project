f25e63e595b65cb6c45bacc9862e0c49a6dfaa96
limjuhee0803 <limjuhee0803@naver.com>
2025-01-25 17:58:27 +0900

    코딩 작업

--- north_face/main/main.js
+++ north_face/main/main.js
@@ -12,7 +12,17 @@ const control = document.getElementById("direction-toggle");
     );
     });
 */
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
 
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+    }
+    
 //상품 오토 스와이퍼
 autoSwiper();
 

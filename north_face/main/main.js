60ffae6c93610a69824a0110a71323211749996b
limjuhee0803 <limjuhee0803@naver.com>
2025-01-28 15:39:41 +0900

    메뉴 추가

--- north_face/main/main.js
+++ north_face/main/main.js
@@ -11,7 +11,60 @@ const lenis = new Lenis({
   }
   requestAnimationFrame(raf);
 */
-  
+
+//header
+/*
+let prevScroll = window.scrollY;
+
+$(window).on('scroll', () => {
+  let currentScroll = window.scrollY;
+
+  if (prevScroll > currentScroll) {
+    $('header').css({ top: 0 });
+  } else {
+    $('header').css({ top: -100 });
+  }
+  prevScroll = currentScroll;
+});
+*/
+
+//menu
+$('.menu_btn').on('click', () => {
+    $('.menu').toggleClass('change-menu');
+    $('.menu .header-icon').toggleClass('change-menu');
+    $('.contents').toggleClass('dark');
+  });
+
+// 스크롤 애니메이션
+$(function(){
+    function scrollevents(scrollPos){
+        var wh =  $(window).height();
+        current_scroll = scrollPos;
+        $(".show_trigger").each(function(){
+            show_whatever($(this), current_scroll, wh);
+        });
+    }
+
+    let ticking = false;
+    var current_scroll, wh;
+    function show_whatever(el, current_scroll, wh){
+        var el_offset_t = el.offset().top;
+        if(current_scroll > el_offset_t - wh + 150){
+            el.parent().removeClass("wait_scroll");
+            el.remove();
+        }    
+    }
+
+    $(window).on('scroll',function(){
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                scrollevents(window.scrollY);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+});
 
 //카드 이펙트
 document.getElementById("cards").onmousemove = e => {
